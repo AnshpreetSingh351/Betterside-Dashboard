38362c60b5c033d105366f01fe6ff1c026e9876e
AnshpreetSingh351 <gujralanshpreetsingh@gmail.com>
2026-03-05 16:12:45 +0530

    k

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
-const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
-dotenv.config();
 connectDB();
 
 const app = express();
